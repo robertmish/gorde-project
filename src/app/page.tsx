90394d5202d22a07fdc3d3e016f44fc75c3f8f6e
robertmish <robertmish@gmail.com>
2023-07-22 07:25:37 +0200

    for  label removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ const onClick = () => {
         </select>
     </div>
     <div className="call_text">email of all new jobs!</div>
-    <div className="email_input"><input for="email" type="text" id="input-group-1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your email"/></div>
+    <div className="email_input"><input type="text" id="input-group-1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your email"/></div>
     <button className='signup_btn'>Subscribe</button>
 </div>
 </div>
